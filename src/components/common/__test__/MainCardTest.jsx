effde51f76e29cd8b0edaf322a77dfc0e4bb1fd1
Josangho99 <josangho99@gmail.com>
2025-08-25 18:49:13 +0900

    feat: mainpage card testCase 추가

--- src/components/common/__test__/MainCardTest.jsx
+++ src/components/common/__test__/MainCardTest.jsx
@@ -10,6 +10,11 @@ function MainCardTest(props) {
         context={`멘토링 신청, 재테크 유형, 서비스 점검 등
 안내사항을 확인하세요.`}
       />
+      <MainCard
+        title="FAQ"
+        context={`결제취소, 환불, 문의 등 주요 질문과 답변을 
+살펴보세요.`}
+      />
     </div>
   );
 }
