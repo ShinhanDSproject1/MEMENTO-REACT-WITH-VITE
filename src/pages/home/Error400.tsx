9b61ab812d3016552f67b8f10052f1a463507a52
danya94-2 <sin_sit@naver.com>
2025-09-19 16:19:55 +0900

    [Fix] 빌드 버그픽스

--- src/pages/home/Error400.tsx
+++ src/pages/home/Error400.tsx
@@ -1,5 +1,5 @@
-import character from "@assets/images/character/character-fox.svg";
-import logo from "@assets/images/logo/memento-logo.svg";
+import character from "@shared/assets/images/character/character-fox.svg";
+import logo from "@shared/assets/images/logo/memento-logo.svg";
 import { useNavigate } from "react-router-dom";
 
 function Error400() {
