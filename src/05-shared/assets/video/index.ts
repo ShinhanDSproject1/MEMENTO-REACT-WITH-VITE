8a829e04d47bedb38217f9677c584e9d757820cb
danya94-2 <sin_sit@naver.com>
2025-09-25 15:35:41 +0900

    [Feat] 중간 commit

--- src/05-shared/assets/video/index.ts
+++ src/05-shared/assets/video/index.ts
@@ -1,7 +1,8 @@
+import backgruond from "./backgruond.mp4";
 import mainVideo from "./mainVideo.mp4";
 import mainVideo2 from "./mainVideo2.mp4";
 import mainVideo3 from "./mainVideo3.mp4";
 import mainVideo4 from "./mainVideo4.mp4";
 import mainVideo5 from "./mainVideo5.mp4";
 
-export { mainVideo, mainVideo2, mainVideo3, mainVideo4, mainVideo5 };
+export { backgruond, mainVideo, mainVideo2, mainVideo3, mainVideo4, mainVideo5 };
